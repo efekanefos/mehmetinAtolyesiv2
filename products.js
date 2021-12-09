9c55388cc38ad780fd6e0824f812d5ab82b5b16d
Efekan EFE <efekanefos@gmail.com>
2021-12-09 21:54:44 +0300

    allProductsUpdate

--- products.js
+++ products.js
@@ -1,15 +1,15 @@
 const products = [
   {
-    name: "İtirazım var kapşonlu sweatshirt",
-    image: "../../images/featuredProducts/productImages/1.jpg",
+    name: "İtirazım Var Kapşonlu Sweatshirt",
+    image: "../../images/featuredProducts/productImages/1-trimmy.jpg",
   },
   {
-    name: "Kişiye özel çizimli plak",
-    image: "../../images/featuredProducts/productImages/2.jpg",
+    name: "Kişiye Özel Çizimli Plak",
+    image: "../../images/featuredProducts/productImages/2-trimmy.jpg",
   },
   {
     name: "Müslüm Gürses Ayraç",
-    image: "../../images/featuredProducts/productImages/3.jpg",
+    image: "../../images/featuredProducts/productImages/3-trimmy.jpg",
   },
   {
     name: "Müslüm Gürses Defter",
@@ -24,76 +24,76 @@ const products = [
     image: "../../images/featuredProducts/productImages/6.jpg",
   },
   {
-    name: "Frida Kahlo oversize tişörtü",
-    image: "../../images/featuredProducts/productImages/7.jpg",
+    name: "Frida Kahlo Oversize T-shirt",
+    image: "../../images/featuredProducts/productImages/7-trimmy.jpg",
   },
   {
-    name: "Frida Kahlo oversize tişörtü",
-    image: "../../images/featuredProducts/productImages/8.jpg",
+    name: "Frida Kahlo Oversize T-shirt",
+    image: "../../images/featuredProducts/productImages/8-trimmy.jpg",
   },
   {
-    name: "Yeşilçam box set",
-    image: "../../images/featuredProducts/productImages/9.jpg",
+    name: "Yeşilçam Box Set",
+    image: "../../images/featuredProducts/productImages/9-trimmy.jpg",
   },
   {
-    name: "Kişiye özel box set hediye kutusu",
-    image: "../../images/featuredProducts/productImages/10.jpg",
+    name: "Kişiye Özel Box Set Hediye Kutusu",
+    image: "../../images/featuredProducts/productImages/10-trimmy.jpg",
   },
   {
-    name: "Kişiye özel box set hediye kutusu",
+    name: "Kişiye Özel Box Set Hediye Kutusu",
     image: "../../images/featuredProducts/productImages/11.jpg",
   },
   {
-    name: "Kişiye özel box set hediye kutusu",
-    image: "../../images/featuredProducts/productImages/12.jpg",
+    name: "Kişiye Özel Box Set Hediye Kutusu",
+    image: "../../images/featuredProducts/productImages/12-trimmy.jpg",
   },
   {
-    name: "Frida Kahlo kupası",
+    name: "Frida Kahlo Kupası",
     image: "../../images/featuredProducts/productImages/13.jpg",
   },
   {
-    name: "kişiye özel çizim çift karakter kupa",
+    name: "Kişiye Özel Çizim Çift Karakter Kupa",
     image: "../../images/featuredProducts/productImages/14.jpg",
   },
   {
-    name: "kişiye özel tek karakter çizim kupa",
+    name: "Kişiye Özel Çizim Çift Karakter Kupa",
     image: "../../images/featuredProducts/productImages/15.jpg",
   },
   {
-    name: "yeşilçam kupa",
-    image: "../../images/featuredProducts/productImages/16.jpg",
+    name: "Yeşilçam Kupa",
+    image: "../../images/featuredProducts/productImages/16-trimmy.jpg",
   },
   {
-    name: "yeşilçam kupa",
-    image: "../../images/featuredProducts/productImages/17.jpg",
+    name: "Yeşilçam Kupa",
+    image: "../../images/featuredProducts/productImages/17-trimmy.jpg",
   },
   {
-    name: "Müslüm Gürses kupa bardak",
-    image: "../../images/featuredProducts/productImages/18.jpg",
+    name: "Müslüm Gürses Kupa Bardak",
+    image: "../../images/featuredProducts/productImages/18-trimmy.jpg",
   },
   {
-    name: "Adana Demirspor ve Adanaspor sevgili kupası",
-    image: "../../images/featuredProducts/productImages/19.jpg",
+    name: "Adana Demirspor ve Adanaspor Sevgili Kupası",
+    image: "../../images/featuredProducts/productImages/19-trimmy.jpg",
   },
   {
-    name: "Baskılı Bez çanta",
-    image: "../../images/featuredProducts/productImages/20.jpg",
+    name: "Baskılı Bez Çanta",
+    image: "../../images/featuredProducts/productImages/20-trimmy.jpg",
   },
   {
-    name: "Heidi ve Peter kupa bardak",
-    image: "../../images/featuredProducts/productImages/21.jpg",
+    name: "Heidi ve Peter Kupa Bardak",
+    image: "../../images/featuredProducts/productImages/21-trimmy.jpg",
   },
   {
-    name: "Müslüm Gürses kupa bardak",
-    image: "../../images/featuredProducts/productImages/22.jpg",
+    name: "Müslüm Gürses Kupa Bardak",
+    image: "../../images/featuredProducts/productImages/22-trimmy.jpg",
   },
   {
-    name: "Müslüm Gürses kupası",
+    name: "Müslüm Gürses Kupası",
     image: "../../images/featuredProducts/productImages/23.jpg",
   },
   {
-    name: "Müslüm Gürses kupası",
-    image: "../../images/featuredProducts/productImages/24.jpg",
+    name: "Müslüm Gürses Kupası",
+    image: "../../images/featuredProducts/productImages/24-trimmy.jpg",
   },
 ];
 
@@ -105,20 +105,23 @@ let productInfos = products.forEach(function (product) {
 
 productsSection__list.innerHTML = products
   .map(function (item) {
-    return `<div>
+    return `<div id="productsSection__card">
             <img
             src=${item.image}
             alt="1"
             />
-            <h3>${item.name}</h3>
-            <button class="featuredProducts--listItem--btn">
+        
+            <h3 id="productsSection--listItem--itemName">${item.name}</h3>
+            <button id="productsSection--listItem--btn" class="featuredProducts--listItem--btn">
               <embed
                 class="featuredProducts--listItem--logo"
+                id="productsSection--listItem--logo"
                 src="images/featuredProducts/shopier_logo_1.svg"
                 srcset="images/featuredProducts/shopier_logo_1.svg"
                 alt="Shopier"
               />
-            </button>
+            </button>            
+      
     
             </div>`;
   })
