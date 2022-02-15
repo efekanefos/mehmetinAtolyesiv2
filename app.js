df7829f0dace8634f3648a0d7a9ae7c543b44abf
Efekan EFE <efekanefos@gmail.com>
2022-02-15 18:46:06 +0300

    newUpdates

--- app.js
+++ app.js
@@ -10,6 +10,33 @@ function activeFunction(event) {
 }
 /* Category Header */
 
-/* Slider */
+/* DoubleContent*/
 
-/* Slider */
+const leftContent = document.querySelector(".doubleContent__left--img");
+const rightContent = document.querySelector(".doubleContent__right--img");
+
+function displayDoubleContent(e) {
+  //* Left Side
+  const slideInAtLeft = window.scrollY + window.innerHeight - leftContent.height * 1.5;
+  const imageBottomLeft = leftContent.offsetTop + leftContent.height * 2.1;
+  const isHalfShownLeft = slideInAtLeft > leftContent.offsetTop;
+  const isNotScrolledPastLeft = window.scrollY < imageBottomLeft;
+  //* Right Side
+  const slideInAtRight = window.scrollY + window.innerHeight - rightContent.height * 1.5;
+  const imageBottomRight = rightContent.offsetTop + rightContent.height * 2.1;
+  const isHalfShownRight = slideInAtRight > rightContent.offsetTop;
+  const isNotScrolledPastRight = window.scrollY < imageBottomRight;
+  if (isHalfShownLeft && isNotScrolledPastLeft) {
+    leftContent.classList.add("active");
+  } else {
+    leftContent.classList.remove("active");
+  }
+  if (isHalfShownRight && isNotScrolledPastRight) {
+    rightContent.classList.add("active");
+  } else {
+    rightContent.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", displayDoubleContent);
+/* DoubleContent*/
--- products.js
+++ products.js
@@ -1,36 +1,47 @@
 const products = [
+  /*
   {
     name: "İtirazım Var Kapşonlu Sweatshirt",
     image: "../../images/featuredProducts/productImages/1-trimmy.jpg",
+    link: "",
   },
+  */
   {
     name: "Kişiye Özel Çizimli Plak",
     image: "../../images/featuredProducts/productImages/2-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=9338734",
   },
   {
     name: "Müslüm Gürses Ayraç",
     image: "../../images/featuredProducts/productImages/3-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=9219892",
   },
   {
     name: "Müslüm Gürses Defter",
     image: "../../images/featuredProducts/productImages/4.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=9219809",
   },
   {
     name: "Müslüm Gürses Minimal Tasarım",
     image: "../../images/featuredProducts/productImages/5.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=9219416",
   },
   {
     name: "Müslüm Gürses Minimal Tasarım",
     image: "../../images/featuredProducts/productImages/6.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=9219404",
   },
   {
     name: "Frida Kahlo Oversize T-shirt",
     image: "../../images/featuredProducts/productImages/7-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=6383881",
   },
   {
     name: "Frida Kahlo Oversize T-shirt",
     image: "../../images/featuredProducts/productImages/8-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=6383850",
   },
+  /*
   {
     name: "Yeşilçam Box Set",
     image: "../../images/featuredProducts/productImages/9-trimmy.jpg",
@@ -47,53 +58,67 @@ const products = [
     name: "Kişiye Özel Box Set Hediye Kutusu",
     image: "../../images/featuredProducts/productImages/12-trimmy.jpg",
   },
+  
+  {
+    name: "Kişiye Özel Box Set Hediye Kutusu",
+    image: "../../images/featuredProducts/productImages/13.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5434197",
+  },
+  */
   {
     name: "Frida Kahlo Kupası",
-    image: "../../images/featuredProducts/productImages/13.jpg",
-  },
-  {
-    name: "Kişiye Özel Çizim Çift Karakter Kupa",
     image: "../../images/featuredProducts/productImages/14.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5434197",
   },
   {
     name: "Kişiye Özel Çizim Çift Karakter Kupa",
     image: "../../images/featuredProducts/productImages/15.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5232326",
   },
   {
-    name: "Yeşilçam Kupa",
+    name: "Kişiye Özel Çizim Tek Karakter Kupa",
     image: "../../images/featuredProducts/productImages/16-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5232250",
   },
   {
     name: "Yeşilçam Kupa",
     image: "../../images/featuredProducts/productImages/17-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5229427",
   },
   {
-    name: "Müslüm Gürses Kupa Bardak",
+    name: "Yeşilçam Kupa",
     image: "../../images/featuredProducts/productImages/18-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5229413",
+  },
+  {
+    name: "Müslüm Gürses Kupa",
+    image: "../../images/featuredProducts/productImages/19-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5224742",
   },
   {
     name: "Adana Demirspor ve Adanaspor Sevgili Kupası",
-    image: "../../images/featuredProducts/productImages/19-trimmy.jpg",
+    image: "../../images/featuredProducts/productImages/20-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5221108",
   },
   {
-    name: "Baskılı Bez Çanta",
-    image: "../../images/featuredProducts/productImages/20-trimmy.jpg",
+    name: "Müslüm Gürses Bez Çanta",
+    image: "../../images/featuredProducts/productImages/21-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5221051",
   },
   {
     name: "Heidi ve Peter Kupa Bardak",
-    image: "../../images/featuredProducts/productImages/21-trimmy.jpg",
-  },
-  {
-    name: "Müslüm Gürses Kupa Bardak",
     image: "../../images/featuredProducts/productImages/22-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5220945",
   },
   {
     name: "Müslüm Gürses Kupası",
     image: "../../images/featuredProducts/productImages/23.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5220702",
   },
   {
     name: "Müslüm Gürses Kupası",
     image: "../../images/featuredProducts/productImages/24-trimmy.jpg",
+    link: "https://www.shopier.com/ShowProductNew/products.php?id=5220351",
   },
 ];
 
@@ -112,9 +137,11 @@ productsSection__list.innerHTML = products
             />
         
             <h3 id="productsSection--listItem--itemName">${item.name}</h3>
+            <a href=${item.link} >
             <button id="productsSection--listItem--btn" class="featuredProducts--listItem--btn">
               
-            </button>            
+            </button> 
+            </a>           
       
     
             </div>`;
